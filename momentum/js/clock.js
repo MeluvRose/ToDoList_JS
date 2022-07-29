b79f71c02359bef304797724046c4822c89fff21
Jinskim <tanggu3910@gmail.com>
2022-07-29 17:32:05 +0900

    Design: edit clock and background
    - 시계의 24시간 형식 표현을 12시간 형식으로 변환
    (Convert the 24-hour formatting of the clock in a
    12-hour format.)
    - 일부 이미지 폴더명 수정
    (Edit some image folder names.)
    - 배경 이미지들을 시간대별로 그룹화한 뒤, 현재 시간대에 맞게
    그룹에서 배경이 무작위로 설정되도록 구현
    (After grouping background images by time zone, it was implemented
    so that the background was randomly set within group that fits
    the current time zone.)

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -2,11 +2,28 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+
+  isAfternoon(date);
+  // const hours = String(date.getHours()).padStart(2, "0");
+  // const minutes = String(date.getMinutes()).padStart(2, "0");
+  // const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  // clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function isAfternoon(date) {
+  let timezone = "AM";
+  let hours = date.getHours();
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  if (hours > 11 && hours < 24) {
+    hours = String(hours - 12).padStart(2, "0");
+    timezone = "PM";
+  } else {
+    hours = String(hours).padStart(2, "0");
+  }
+  clock.innerText = `${timezone} ${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // Immediately, to see the current time
